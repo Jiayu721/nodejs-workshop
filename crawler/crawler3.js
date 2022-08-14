cf1322cecac2f0376527e89f0a8d198db5ccc9b7
Jiayu <justine0816123@gmail.com>
2022-08-14 12:55:26 +0800

    update

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,15 +2,15 @@
 // 2. 從檔案中讀取股票代碼
 const axios = require('axios');
 const moment = require('moment');
+const fsPromises = require('fs').promises;
 
-// 開始抓資料
-// 2330 台積電
-// 2603 長榮
-let stockNo = 2603; // TODO: 需要從 stock.txt 的檔案裡讀取股票代碼
 let queryDate = moment().format('YYYYMMDD'); //'20220814';
-// axios.get(url, 設定)
+
+
 (async () => {
   try {
+    let stockNo = await fsPromises.readFile('stock.txt', 'utf8');
+    
     let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
       params: {
         response: 'json',
